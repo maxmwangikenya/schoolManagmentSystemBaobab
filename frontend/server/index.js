791d7479c986af10a471914ded0db72698226d17
maxmwangikenya <maxmwangikenya@gmail.com>
2025-06-12 14:34:17 +0300

    backend added

---
+++ frontend/server/index.js
@@ -0,0 +1,11 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.listen(process.env.PORT, () => {
+    console.log(`server is Running on port ${process.env.PORT}`) // Note: Fixed typo here
+})

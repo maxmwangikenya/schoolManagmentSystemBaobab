1e7d7d67f5ddfd4e6da0ea5fb868dab43146922e
maxmwangikenya <maxmwangikenya@gmail.com>
2025-07-21 13:54:04 +0300

    auth

---
+++ frontend/server/controllers/authController.js
@@ -0,0 +1,6 @@
+
+const login = async(req, res) => {
+
+}
+
+export {login}
--- frontend/server/index.js
+++ frontend/server/index.js
@@ -1,10 +1,12 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import authRouter from './routes/auth.js'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use('/api/auth', authRouter)
 
 app.listen(process.env.PORT, () => {
     console.log(`server is Running on port ${process.env.PORT}`) // Note: Fixed typo here

8da8ebba8ac4d85fa7166d2f8ab82cb3f3840dca
maxmwangikenya <maxmwangikenya@gmail.com>
2025-10-07 16:22:45 +0300

    cors changes

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -19,7 +19,10 @@ const app = express();
 const __dirname = path.resolve();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://schoolmanagmentsystembaobab.onrender.com'|| 'http://localhost:5173',
+  credentials: true
+}));
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));

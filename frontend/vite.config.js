75d39b742b457fa748994a65867a470e649db8bf
maxmwangikenya <maxmwangikenya@gmail.com>
2025-11-06 13:47:55 +0300

    removed build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,22 +4,22 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-  "version": 2,
-  "builds": [
-    {
-      "src": "package.json",
-      "use": "@vercel/static-build",
-      "config": {
-        "distDir": "dist"
-      }
-    }
-  ],
-  "routes": [
-    {
-      "src": "/(.*)",
-      "dest": "/"
-    }
-  ]
-}
+//   build: {
+//   "version": 2,
+//   "builds": [
+//     {
+//       "src": "package.json",
+//       "use": "@vercel/static-build",
+//       "config": {
+//         "distDir": "dist"
+//       }
+//     }
+//   ],
+//   "routes": [
+//     {
+//       "src": "/(.*)",
+//       "dest": "/"
+//     }
+//   ]
+// }
 })
